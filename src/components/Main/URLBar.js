b8ffc3d65c497868def8952beee4dfe1b5b0fdeb
jetsabel <jetsabel@gmail.com>
2018-06-18 15:52:37 +1000

    test branching 2

--- src/components/Main/URLBar.js
+++ src/components/Main/URLBar.js
@@ -5,7 +5,7 @@ class URLBar extends React.Component {
   render() {
     return <React.Fragment>
       <div className="center pa3 ph5-ns">
-          <input onDrop={this.props.onDrop} onChange={this.props.onInputChange} placeholder="paste image URL here" className="w-70 f4 link ph3 pv2 dib " type="text" />
+          <input onDrop={this.props.onDrop} onChange={this.props.onInputChange} placeholder="drop image tag here" className="w-70 f4 link ph3 pv2 dib " type="text" />
           <button onClick={this.props.onLoadImage} className="w-30 f4 link ph3 pv2 dib white bg-light-purple">
             {" "}
             Load{" "}
