0fc0902cfd68b92e351857e49d25be0db462e58a
jetsabel <jetsabel@gmail.com>
2018-06-18 16:04:22 +1000

    cleaned up main and URLBar

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,34 +1,22 @@
 import React from "react";
 import "../../index.css";
-import Helpers from './Helpers'
+import Helpers from "./Helpers";
 import URLBar from "./URLBar";
 // import Rank from "./Rank";
 import DisplayImage from "./DisplayImage";
-// import FaceRecognition from "./FaceRecognition";
 import Clarifai from "clarifai";
-import Dropzone from 'react-dropzone'
-import DefaultImage from '../Logo/Logo'
-import ConditionGame from './ConditionGame'
+import Dropzone from "react-dropzone";
+import DefaultImage from "../Logo/Logo";
+import ConditionGame from "./ConditionGame";
 
 const app = new Clarifai.App({
   apiKey: "f43a1b9e565d4b5abec85b8a823bf2b5"
 });
 
-
 class Main extends React.Component {
   constructor() {
     super();
-    this.state = {
-      // input: "",
-      // // imageURL: 'https://i.ebayimg.com/images/g/3y8AAOSwuMZZAqcE/s-l300.jpg',
-      // // input: 'https://i.ebayimg.com/images/g/3y8AAOSwuMZZAqcE/s-l300.jpg',
-      // // answerFace: undefined,
-      // answerFaceNumber: undefined,
-      // answerDomCol: undefined,
-      // answerNSFW: undefined,
-      // onLoadImage: undefined,
-      // onInputChange: undefined
-    };
+    this.state = {};
   }
 
   setInitialState = () => {
@@ -38,7 +26,6 @@ class Main extends React.Component {
   };
 
   onLoadImage = event => {
-    // console.log('onLoadImage()',event)
     this.setInitialState();
     this.setState({ imageURL: this.state.input });
     console.log("onLoadImage()");
@@ -48,13 +35,10 @@ class Main extends React.Component {
   onDrop = evt => {
     evt.stopPropagation();
     evt.preventDefault();
-    var imageUrl = evt.dataTransfer.getData('URL');
-    this.setState({input: imageUrl})
-    // this.onLoadImage()
-    // this.getRawData()
-  }
+    var imageUrl = evt.dataTransfer.getData("URL");
+    this.setState({ input: imageUrl });
+  };
   getRawData = () => {
-    // console.log("getRawData()",this.state.input);
     let temp_answerGeneral,
       temp_answerNSFW,
       temp_answerFood,
@@ -108,39 +92,46 @@ class Main extends React.Component {
         console.log(response);
         temp_answerModeration = response.outputs[0].data.concepts;
       },
-      function(err) {
-        // alert("error - valid image URL and web connection?");
-      }
+      function(err) {}
     );
     setTimeout(() => {
-      let raw = {}
-      raw.nSFW= temp_answerNSFW 
-      raw.general= temp_answerGeneral 
-      raw.moderation= temp_answerModeration 
-      raw.colors= temp_answerDomCol 
-      raw.faces= temp_answerFaces 
-      raw.food= temp_answerFood 
+      let raw = {};
+      raw.nSFW = temp_answerNSFW;
+      raw.general = temp_answerGeneral;
+      raw.moderation = temp_answerModeration;
+      raw.colors = temp_answerDomCol;
+      raw.faces = temp_answerFaces;
+      raw.food = temp_answerFood;
       this.setState({ raw: raw });
       this.processData();
     }, 3000);
   };
 
   processData = () => {
-let thresholdGeneral = 0.95
-let thresholdModeration = 0.95
-let thresholdFood = 0.90
-let thresholdDomCol = 0.33
-let answers = {}
-        if(this.state.raw.general) answers.general = this.state.raw.general.filter(element => element.value>thresholdGeneral)
-        if(this.state.raw.moderation) answers.moderation = this.state.raw.moderation.filter(element => element.value>thresholdModeration)
-        if(this.state.raw.colors) answers.colors = this.state.raw.colors.filter(element => element.value>thresholdDomCol)
-        if(this.state.raw.faces) answers.faces = this.state.raw.faces
-        if(this.state.raw.food) answers.food = this.state.raw.food.filter(element => element.value>thresholdFood)
-      this.setState({ answers: answers });
-  }
-
-
-
+    let thresholdGeneral = 0.95;
+    let thresholdModeration = 0.95;
+    let thresholdFood = 0.9;
+    let thresholdDomCol = 0.33;
+    let answers = {};
+    if (this.state.raw.general)
+      answers.general = this.state.raw.general.filter(
+        element => element.value > thresholdGeneral
+      );
+    if (this.state.raw.moderation)
+      answers.moderation = this.state.raw.moderation.filter(
+        element => element.value > thresholdModeration
+      );
+    if (this.state.raw.colors)
+      answers.colors = this.state.raw.colors.filter(
+        element => element.value > thresholdDomCol
+      );
+    if (this.state.raw.faces) answers.faces = this.state.raw.faces;
+    if (this.state.raw.food)
+      answers.food = this.state.raw.food.filter(
+        element => element.value > thresholdFood
+      );
+    this.setState({ answers: answers });
+  };
 
   onInputChange = event => {
     this.setState({ input: event.target.value });
@@ -148,7 +139,6 @@ let answers = {}
   render() {
     return (
       <React.Fragment>
-        {/* <div className="form center pa4 br3 shadow-5 bg-stripeypattern w-50"> */}
         <div className="center pa4 br3 shadow-5 bg-stripeypattern w-50">
           <URLBar
             onInputChange={this.onInputChange}
@@ -156,85 +146,13 @@ let answers = {}
             onLoadImage={this.onLoadImage}
             onDrop={this.onDrop}
           />
-          {/* <Dropzone /> */}
-
-          {/* <Dropzone
-            accept="image/jpeg, image/png image/gif"
-            onDrop={(accepted, rejected) => {
-              this.setState({ accepted, rejected });
-            }}
-          /> */}
-
-          {/* <Dropzone onDrop={this.onDrop.bind(this)} disabled={this.state.disabled}>
-            <p>Try dropping some files here, or click to select files to upload.</p>
-          </Dropzone> */}
         </div>
-        <div className="center pa4 br3 shadow-5 w-50">
-          <ConditionGame state={this.state}/>
-      </div>
-        {/* <div className="center pa4 br3 shadow-5 bg-stripeypattern w-50"> */}
-        {/* <DisplayImage imageURL={this.props.imageURL} /> */}
-        {/* <img src={this.state.imageURL||DefaultImage} alt="logo" height="auto" width="500px" />  */}
-        {/* </div> */}
+        <div className="center pa4 br3 shadow-5 w-50 bg-light-purple tc op-5">
+          <ConditionGame state={this.state} />
+        </div>
       </React.Fragment>
     );
   }
 }
-
-
-
-
-
-// class Accept extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       accepted: [],
-//       rejected: []
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <section>
-//         <div className="dropzone">
-//           <Dropzone
-//             accept="image/jpeg, image/png image/gif"
-//             onDrop={(accepted, rejected) => {
-//               this.setState({ accepted, rejected });
-//             }}
-//           >
-//             <p>
-//               Try dropping some files here, or click to select files to upload.
-//             </p>
-//             <p>Only *.jpeg, *.gif and *.png images will be accepted</p>
-//           </Dropzone>
-//         </div>
-//         <aside>
-//           <h2>Accepted files</h2>
-//           <ul>
-//             {this.state.accepted.map(f => (
-//               <li key={f.name}>
-//                 {f.name} - {f.size} bytes
-//               </li>
-//             ))}
-//           </ul>
-//           <h2>Rejected files</h2>
-//           <ul>
-//             {this.state.rejected.map(f => (
-//               <li key={f.name}>
-//                 {f.name} - {f.size} bytes
-//               </li>
-//             ))}
-//           </ul>
-//         </aside>
-//       </section>
-//     );
-//   }
-// }
-//<Accept />;
-
-
-
 
 export default Main;
--- src/components/Main/URLBar.js
+++ src/components/Main/URLBar.js
@@ -3,10 +3,20 @@ import DisplayImage from "./DisplayImage";
 
 class URLBar extends React.Component {
   render() {
-    return <React.Fragment>
-      <div className="center pa3 ph5-ns">
-          <input onDrop={this.props.onDrop} onChange={this.props.onInputChange} placeholder="drop image tag here" className="w-70 f4 link ph3 pv2 dib " type="text" />
-          <button onClick={this.props.onLoadImage} className="w-30 f4 link ph3 pv2 dib white bg-light-purple">
+    return (
+      <React.Fragment>
+        <div className="center pa3 ph5-ns">
+          <input
+            onDrop={this.props.onDrop}
+            onChange={this.props.onInputChange}
+            placeholder="drop image tag here"
+            className="w-70 f4 link ph3 pv2 dib "
+            type="text"
+          />
+          <button
+            onClick={this.props.onLoadImage}
+            className="w-30 f4 link ph3 pv2 dib white bg-light-purple"
+          >
             {" "}
             Load{" "}
           </button>
@@ -14,42 +24,8 @@ class URLBar extends React.Component {
         </div>
         {/* <div className="center"> */}
         {/* </div> */}
-      </React.Fragment>;
+      </React.Fragment>
+    );
   }
 }
 export default URLBar;
-
-// import React from "react";
-// // import "./URLBar.css";
-
-// const URLBar = ({ onInputChange, onLoadImage, imageURL }) => {
-//   return (
-//     <React.Fragment>
-//       <div className="flex ">
-//         <input
-//           onChange={onInputChange}
-//           placeholder="paste image URL here"
-//           className="f4 pa2 w-70 center"
-//           type="text"
-//         />
-//         <button
-//           onClick={onLoadImage}
-//           className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
-//         >
-//           Load Image
-//     </button>
-//       </div>
-//       {/* <div className="flex-wrap center ">
-//       <img className="center ma absolute mt2 flex flex-wrap block"
-//         id="inputImage"
-//         src={imageURL}
-//         alt="img"
-//         width="500px"
-//         height="auto"
-//       />
-//         </div> */}
-//     </React.Fragment>
-//   );
-// };
-
-// export default URLBar;
